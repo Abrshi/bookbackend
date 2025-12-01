f0f4bc722da96b47cb83ecdb379534d114397778
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2025-12-01 05:34:23 -0800

    now backend can revre book

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -24,14 +24,14 @@ const hashToken = (token) =>
 const accessCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: 5 * 60 * 1000, // 5 minutes
 };
 
 const refreshCookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
